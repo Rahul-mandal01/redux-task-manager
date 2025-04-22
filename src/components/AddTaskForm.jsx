8b4a07ad9aeab0309ffd2225ca5c03116eadacb1
rahul <rahulra16809999@gmail.com>
2025-04-22 20:01:33 +0530

    feat: implement AddTaskForm component for adding tasks
    Created a form to input and submit new tasks.
    Integrated useDispatch to dispatch the addTask action to the Redux store.
    Added input validation to prevent empty tasks from being submitted.
    Styled the form and button using Tailwind CSS.

---
+++ src/components/AddTaskForm.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addTask } from "../features/tasks/taskSlice";
+
+const AddTaskForm = () => {
+  const [task, setTask] = useState("");
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!task.trim()) return;       //If the task is blank, ignore and don't do anything. trim() removes whitespace)
+    dispatch(addTask(task));
+    setTask("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="mb-4 gap-7 ">
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        placeholder="Enter task"
+        className="border p-2 rounded mr-2"
+      />
+      <button
+        type="submit"
+        className="bg-blue-500 text-white px-3 py-2 ml-5 rounded"
+      >
+        Add Task
+      </button>
+    </form>
+  );
+};
+
+export default AddTaskForm;
